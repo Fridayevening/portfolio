a75c14a4c8e48fc44d9046dc9b5cbfe7e85f3892
Fridayevening <wyf024326@gmail.com>
2025-01-31 16:22:14 +0800

    Deploy updated Vue app to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/portfolio/',  // 根据你的 GitHub 仓库名设置路径
 })
